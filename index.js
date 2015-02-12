f4239f0a691d3b914d83d1093b288ae8ba865fde
Taylor Hobbs <taylor@feemon.com>
2015-02-11 19:17:10 -0600

    Feature: Add tomster-cli to background of parent app

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
 
 module.exports = {
-  name: 'tomster-cli'
+  name: 'tomster-cli',
+  treeForPublic: function(tree) {
+    var publicDir = path.join(this.project.root, 'public');
+    var publicFiles = this.pickFiles(this.treeGenerator(publicDir), {
+      srcDir: '/',
+      destDir: '/'
+    });
+    if (tree) {
+      return this.mergeTrees([tree, publicFiles], {overwrite: true});
+    }
+    else {
+      return publicFiles;
+    }
+  },
+  included: function(app) {
+    app.import('vendor/app.css');
+  }
 };
