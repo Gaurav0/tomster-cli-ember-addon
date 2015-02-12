fc21ac20345fc1badecb6e2a9663ceea582fa512
Taylor Hobbs <taylor@feemon.com>
2015-02-11 19:34:12 -0600

    Feature: include tomster-cli in the head of index.html

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var path = require('path');
 
 module.exports = {
   name: 'tomster-cli',
+  contentFor: function(type, config) {
+    var content = [];
+    if ( type === 'head' ) {
+      content.push('<link href="/assets/images/tomster_cli.png" type="image/x-icon">');
+    }
+    return content.join('\n');
+  },
   treeForPublic: function(tree) {
     var publicDir = path.join(this.project.root, 'public');
     var publicFiles = this.pickFiles(this.treeGenerator(publicDir), {
